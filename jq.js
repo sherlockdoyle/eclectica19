efa16d77e478ebf6022232047c9d6f5def4a6ae8
ritik17499 <ritikapril17@gmail.com>
2018-10-24 22:38:15 +0530

    my_commit

---
+++ jq.js
@@ -0,0 +1,39 @@
+(function ($) {
+  $(document).ready(function(){
+    
+	// hide .navbar first
+	$(".navbar").hide();
+	
+	// fade in .navbar
+	$(function () {
+		$(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+			if ($(this).scrollTop() > 100) {
+				$('.navbar').fadeIn();
+			} else {
+				$('.navbar').fadeOut();
+			}
+		});
+
+	
+	});
+window.onscroll = function() {myFunction()};
+
+
+var navbar = document.getElementById("navu");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+});
+  }(jQuery));
