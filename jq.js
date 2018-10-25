779b038efa443ff40158ca994a76ee9506380e3b
ritik17499 <42418456+ritik17499@users.noreply.github.com>
2018-10-25 22:27:18 +0530

    Update jq.js

--- jq.js
+++ jq.js
@@ -1,17 +1,20 @@
 (function ($) {
   $(document).ready(function(){
     
-	// hide .navbar first
-	$(".navbar").hide();
 	
-	// fade in .navbar
+
+
+	$(".bg-text").hide();
+	
+
+
+	
 	$(function () {
 		$(window).scroll(function () {
-            // set distance user needs to scroll before we fadeIn navbar
 			if ($(this).scrollTop() > 100) {
-				$('.navbar').fadeIn();
+				$('.bg-text').fadeIn();
 			} else {
-				$('.navbar').fadeOut();
+				$('.bg-text').fadeOut();
 			}
 		});
 
@@ -22,10 +25,8 @@ window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navu");
 
-// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
